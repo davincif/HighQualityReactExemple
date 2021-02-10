5e524d126ea176da756b502438b695f835b93df8
davincif <LDaVinciF@gmail.com>
2021-02-10 10:58:38 -0300

    fix: server must fail in invalid date

--- server/graphql/scalars/date.ts
+++ server/graphql/scalars/date.ts
@@ -5,6 +5,11 @@ export const dateScalar = new GraphQLScalarType({
   name: "Date",
   description: "Date custom scalar type",
   serialize(value: Date) {
+    // value is a invalid date
+    if (isNaN(value.getTime())) {
+      throw new Error("Date is invalid");
+    }
+
     return `${value.getDate()}/${value.getMonth()}/${value.getFullYear()}`;
   },
   parseValue(value) {
